1381ab238c5b2513771f726a394d711e2e8d62f0
Rayal Khatri <kcrayalgw@gmail.com>
2026-01-04 10:32:07 +0545

    Crispy Forms initilization

--- corporate_portal/corporate_portal/settings.py
+++ corporate_portal/corporate_portal/settings.py
@@ -28,7 +28,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_system',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- corporate_portal/main_system/views.py
+++ corporate_portal/main_system/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponse
+
+
+class CompanyLoginForm(forms.Form):
+    company_id = forms.CharField(
+        label="Company ID",
+        max_length=20,
+        help_text="Enter your unique RJBCL Company ID"
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput,
+        label="Password"
+    )
+
 
 def home(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = CompanyLoginForm(request.POST)
+        if form.is_valid():
+            # For testing purposes only
+            return HttpResponse("Form is valid! Crispy forms is working.")
+    else:
+        form = CompanyLoginForm()
+
+    return render(request, 'index.html', {'form': form})
