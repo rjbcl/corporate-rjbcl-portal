95d875e4964f3fbd60a1f6f8832af98f52b61fcf
Rayal Khatri <kcrayalgw@gmail.com>
2026-01-14 15:35:14 +0545

    test

--- corporate_portal/corporate_portal/urls.py
+++ corporate_portal/corporate_portal/urls.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.contrib import admin #type: ignore
+from django.urls import path, include #type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls),
