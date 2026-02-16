6f5edd8616716fe94bd367201cee5ac20b9ed518
Rayal Khatri <kcrayalgw@gmail.com>
2026-02-16 16:41:10 +0545

    removed unwanted columns

--- Database Scripts/view_copo_GroupEndowment.sql
+++ Database Scripts/view_copo_GroupEndowment.sql
@@ -1,119 +1,41 @@
-CREATE OR ALTER VIEW dbo.view_copo_groupEndowment
-WITH SCHEMABINDING
+CREATE OR ALTER VIEW view_copo_policySummary
 AS
 SELECT 
-    -- Join Keys
-    ge.Branch,
-    ged.RegisterNo,
-    ged.PolicyNo,
-    
-    -- Fields prioritizing tblGroupEndowmentDetails (more reliable)
-    ged.GroupId as GroupId,
-    ged.DOC as DOC,
-    ged.Term as Term,
-    ged.SumAssured as SumAssured,
-    ged.Premium as Premium,
-    ged.FUP as FUP,
-    ged.MaturityDate as MaturityDate,
-    ged.PolicyStatus as PolicyStatus,
-    ged.PolicyType as PolicyType,
-    ged.LateFine as LateFine,
-    
-    -- Fields unique to tblGroupEndowmentDetails
-    ged.PaidDate,
-    ged.Instalment,
-    ged.PaidAmount,
-    ged.BatchNo,
-    ged.Remarks as DetailsRemarks,
-    ged.Intrest,
-    ged.ClaimStatus,
-    ged.LateFinePercent,
-    ged.ReducedInstalment,
-    
-    -- Fields unique to tblGroupEndowment
-    ge.EmployeeId,
-    ge.Name,
-    ge.NepName,
-    ge.Gender,
-    ge.Occupation,
-    ge.DOB,
-    ge.Age,
-    ge.ExtraPremium,
-    ge.TotalPremium,
-    ge.IdNo,
-    ge.IdType,
-    ge.AppointedDate,
-    ge.Remarks as EndowmentRemarks,
-    ge.Address,
-    ge.Email,
-    ge.Mobile,
-    ge.ADB,
-    ge.PreviousPolicy,
-    ge.OccExtraAmount,
-    ge.ADBDiscount,
-    ge.FatherName,
-    ge.MotherName,
-    ge.NomineeName,
-    ge.NomineeAddress,
-    ge.PhoneNumberResidence,
-    ge.TransferDate,
-    ge.DuplicatePolicyDate,
-    ge.ApprovedDate,
-    ge.ApprovedBy,
-    ge.LapseDate,
-    ge.LapseActiveDate,
-    ge.DOE,
-    ge.ApproveRemarks,
-    ge.ModifiedDate,
-    ge.BasicPremium,
-    ge.IsADB,
-    ge.AfterDisRebateRate,
-    ge.FiscalYear,
-    ge.NomineeRelationship,
-    ge.ClaimDate,
-    ge.TerminationDate,
-    ge.IsINDIssue,
-    ge.ProvinceID,
-    ge.DistrictID,
-    ge.MunicipalityID,
-    ge.WardNo,
-    ge.AgeProofDocType,
-    ge.AgeProofDocNo,
-    ge.NepAddress,
-    ge.NepFatherName,
-    ge.NepMotherName,
-    ge.NepNomineeName,
-    ge.NepNomineeAddress,
-    ge.NomDistrictID,
-    ge.NomineeWardNo,
-    ge.NomineePhone,
-    ge.PlanId,
-    ge.IsMultiplePolicyIssued,
-    ge.TerminateBy,
-    ge.CancelDate,
-    ge.CancelBy,
-    ge.ActiveDate,
-    ge.ActiveBy,
-    ge.TerminateRemarks,
-    ge.CancelRemarks,
-    ge.ActiveRemarks,
-    ge.LapseBy,
-    ge.LapseRemarks
-    
-FROM [dbo].[tblGroupEndowment] ge
-INNER JOIN [dbo].[tblGroupEndowmentDetails] ged
-    ON ge.RegisterNo = ged.RegisterNo 
-    AND ge.PolicyNo = ged.PolicyNo;
-GO
--- Now create the unique clustered index
-CREATE UNIQUE CLUSTERED INDEX IX_view_copo_groupEndowment
-ON dbo.view_copo_groupEndowment (RegisterNo, PolicyNo);
-
---Remarks Fields: Renamed to DetailsRemarks and EndowmentRemarks to distinguish them
-
-
-
-
--- SELECT TOP 10 * FROM view_copo_groupEndowment;
-
--- SELECT COUNT(*) as TotalRecords FROM GroupEndowment_View;
+    a.registerNo,
+    a.PolicyNo,
+    a.Branch,
+    a.Name,
+    a.NepName,
+    a.GroupId,
+    a.DOB,
+    a.Gender,
+    a.Address,
+    a.Email,
+    a.Mobile,
+    a.FatherName,
+    a.MotherName,
+    a.NomineeName,
+    a.NomineeRelationship,
+    a.ClaimDate,
+    a.DistrictID,
+    a.WardNo,
+    a.NomineePhone,
+    a.NomineeAddress,
+    COALESCE(s.Value, a.Occupation) AS Occupation,
+    b.Sumassured,
+    b.DOC,
+    b.PaidDate,
+    b.FUP,
+    b.Term,
+    b.Premium,
+    b.Instalment,
+    b.PaidAmount,
+    b.maturitydate,
+    b.PolicyStatus,
+    b.PolicyType
+FROM tblGroupEndowment AS a
+INNER JOIN tblgroupEndowmentDetails AS b  
+    ON a.PolicyNo = b.PolicyNo 
+    AND a.registerNo = b.registerNo
+LEFT JOIN tblStaticDataValue AS s
+    ON TRY_CAST(a.Occupation AS INT) = s.Id;
